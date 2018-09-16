b7f2331b10b3497541b538f653c59a20d2bf029a
chenjinxinlove <505150704@qq.com>
2018-09-16 21:53:24 +0800

    queue

--- Java/队列Queue/src/Main.java
+++ Java/队列Queue/src/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int opCount = 10000000;
+        int opCount = 100000;
 
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
         double time1 = testQueue(arrayQueue, opCount);
