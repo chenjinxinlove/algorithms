f2e2013dc763b4f7df8e0860ec8eb446f84e5978
chenjinxinlove <505150704@qq.com>
2018-09-16 19:43:50 +0800

    queue

--- Java/栈Stack/src/ArrayStack.java
+++ Java/栈Stack/src/ArrayStack.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+public class ArrayStack<E> implements Stack<E> {
+    Array<E> array;
 
-public class ArrayStack {
+    public ArrayStack(int capacity) {
+        array = new Array<>(capacity);
+    }
+    public ArrayStack() {
+        array = new Array<>();
+    }
+
+    @Override
+    public int getSize(){
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return array.isEmpty();
+    }
+
+    public int getCapacity(){
+        return array.getCapacity();
+    }
+
+    @Override
+    public void push(E e) {
+        array.addLast(e);
+    }
+    @Override
+    public E pop() {
+        return  array.removeLast();
+    }
+
+    @Override
+    public E peek() {
+        return array.getLast();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Stack: "));
+        res.append('[');
+        for(int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i !=  array.getSize() -1)
+                res.append(", ");
+        }
+        res.append("] top");
+        return res.toString();
+    }
 }
