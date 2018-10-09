3e1730227af7c14e3a0df92ea80682a78642f4f2
chenjinxinlove <505150704@qq.com>
2018-10-09 22:27:21 +0800

    tire

---
+++ Java/Tire/src/Trie.java
@@ -0,0 +1,82 @@
+import com.sun.org.apache.bcel.internal.generic.RET;
+
+import java.util.TreeMap;
+
+public class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public Trie(){
+        root = new Node();
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 添加
+     * @param word
+     */
+    public void add(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length() ; i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        if (!cur.isWord) {
+            cur.isWord = true;
+            size ++;
+        }
+    }
+
+    /**
+     * 是否包含
+     * @param word
+     * @return
+     */
+    public boolean contains(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    /**
+     * 查询是否遭tire中有单词以prefix为前缀
+     * @param prefix
+     * @return
+     */
+    public boolean isPrefix(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
