00278be07e3f4fa66a5d16fca436be4965d8dce2
陈金鑫 <505150704@qq.com>
2018-11-21 19:04:20 +0800

    fibonacci

--- Exercise/recursion.js
+++ Exercise/recursion.js
@@ -37,41 +37,41 @@
 
 
 
-// /**
-//  * Definition for singly-linked list.
-//  * function ListNode(val) {
-//  *     this.val = val;
-//  *     this.next = null;
-//  * }
-//  */
-// /**
-//  * @param {ListNode} head
-//  * @return {ListNode}
-//  */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 
-// var reverseList = function(head) {
-//     var prev = null;
-//     var cur = head;
-//     var temp = null;
-//     while (cur !== null) {
-//         temp = cur.next;
-//         cur.next = prev;
-//         prev = cur;
-//         cur = temp;
-//     }
-//     return prev;
-// }
+var reverseList = function(head) {
+    var prev = null;
+    var cur = head;
+    var temp = null;
+    while (cur !== null) {
+        temp = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = temp;
+    }
+    return prev;
+}
 
-// // 链表的递归实现
-// var reverseList = function(H) {
-// 	if (H === null || H.next === null)
-// 		//链表为空直接返回，而H->next为空是递归基
-// 		return H
-// 	var newHead = reverseList(H.next) //一直循环到链尾
-// 	H.next.next = H //翻转链表的指向
-// 	H.next = null //记得赋值NULL，防止链表错乱
-// 	return newHead //新链表头永远指向的是原链表的链尾
-// }
+// 链表的递归实现
+var reverseList = function(H) {
+	if (H === null || H.next === null)
+		//链表为空直接返回，而H->next为空是递归基
+		return H
+	var newHead = reverseList(H.next) //一直循环到链尾
+	H.next.next = H //翻转链表的指向
+	H.next = null //记得赋值NULL，防止链表错乱
+	return newHead //新链表头永远指向的是原链表的链尾
+}
 
 function fibonacci_nornal(n) {
     if (n === 0) {
