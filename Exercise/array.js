6e2a41590ecac7f60f1b7a81a030112a4c410c74
chenjinxinlove <505150704@qq.com>
2018-11-30 22:34:03 +0800

    min

--- Exercise/array.js
+++ Exercise/array.js
@@ -20,3 +20,31 @@ var intersect = function(nums1, nums2) {
 };
 
 intersect([1,2,3,4,4,1], [1,2,4,4])
+
+
+
+
+var minSubArrayLen = function(s, nums) {
+	let l = 0
+	let r = -1 // 滑动窗口 nums[i...j]
+	let sum = 0 // 记录找到的最大值
+	let res = nums.length + 1 // 记录连续数组的长度
+
+	while (l < nums.length) {
+		if (r + 1 < nums.length && sum < s) {
+			r++
+			sum += nums[r]
+		} else {
+            sum -= nums[l]
+            l++;
+		}
+		if (sum >= s) {
+			res = Math.min(res, r - l + 1)
+		}
+	}
+	if (res === nums.length) {
+		return 0 // 没有找到答案返回0
+	}
+	return res
+}
+minSubArrayLen(7, [2, 3, 1, 2, 4, 3])
