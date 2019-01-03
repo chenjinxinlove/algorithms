a4c260f96e9dfe9159bb878ec68f22eeb0baa4d9
chenjinxinlove <505150704@qq.com>
2019-01-03 16:32:50 +0800

    nfs

---
+++ Leecode/102.js
@@ -0,0 +1,11 @@
+var levelOrder = function(root) {
+  let res = ''
+  if (root === null) {
+    return res
+  }
+  let q = []
+  q.push({node: root,index: 0})
+  while (!q.length) {
+    let node = q.unshift()
+  }
+};
